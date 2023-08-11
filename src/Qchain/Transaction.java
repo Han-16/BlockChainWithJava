5cfffe85fd0ac216573e9b9bb06234d7c3e919cf
Han-16 <haanbk16@gmail.com>
2023-08-11 17:55:55 +0900

    first commit

---
+++ src/Qchain/Transaction.java
@@ -0,0 +1,36 @@
+package Qchain;
+
+import java.security.PublicKey;
+import java.util.ArrayList;
+
+public class Transaction {
+
+    public String transactionId; // this is also the hash of the transaction.
+    public PublicKey sender; // senders address/public key.
+    public PublicKey reciepient; // Recipients address/public key.
+    public float value;
+    public byte[] signature; // this is to prevent anybody else from spending funds in our wallet.
+
+    public ArrayList<TransactionInput> inputs = new ArrayList<>();
+    public ArrayList<TransactionOutput> outputs = new ArrayList<>();
+
+    private static int sequence = 0;
+
+    // Constructor
+    public Transaction(PublicKey from, PublicKey to, float value, ArrayList<TransactionInput> inputs) {
+        this.sender = from;
+        this.reciepient = to;
+        this.value = value;
+        this.inputs = inputs;
+    }
+
+    // This Calculates the transaction hash which will be used as its Id
+    private String calculateHash() {
+        sequence ++;
+        return StringUtil.applySha256(
+                StringUtil.getStringFromKey(sender)
+                + StringUtil.getStringFromKey(reciepient)
+                + Float.toString(value) + sequence
+        );
+    }
+}
